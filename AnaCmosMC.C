aff40053da25ca96c597100c95db92705f8f4817
FRANCESCO COLLAMATI <francesco.collamati@roma1.infn.it>
2018-01-18 15:59:31 +0100

    Deep update of AnaCMOS, now working with both sensors and checked

--- AnaCmosMC.C
+++ AnaCmosMC.C
@@ -8,8 +8,15 @@
 //###############################################
 //##### Scopo della macro:
 // L'output della simulazione GEANT4 è scritto evento per evento, mentre i dati sono scritti frame per frame. Conoscendo l'attività totale della sorgente è possibile ricostruire una struttura di frame anche per il MC
-
-
+//### Produce in output:
+// - un file XXX_Frame_NNN.root con la struttura di frame, da mandare alla stessa procedura di analisi dei dati
+// - un file XXX_Image.root, che contiene un TH2F per ogni frame con il segnale di quel frame, piu un TH2F globale con la sovrapposizone di tutti i segnali di tutti i frame
+// USE:
+/*
+.L AnaCmosMC.C
+c=new AnaCmosMC("/Users/francesco/MonteCarlo/Sonda/SimCMOS/build/CMOSmcX0_Z2_NOCuD_Fil0_TBR10_DOTA_115")
+c->Loop()
+*/
 
 #define AnaCmosMC_cxx
 #include "AnaCmosMC.h"
@@ -21,7 +28,7 @@
 #include "Frame.C"
 #include "Seed.C"
 #include "SeedList.C"
-
+#include <iostream>
 
 void AnaCmosMC::Loop()
 {
@@ -31,21 +38,23 @@ void AnaCmosMC::Loop()
 	Long64_t nentries = fChain->GetEntriesFast();
 	
 	int SensorChoice=2; //1 is MT9V011, 2 is MT9V115
-	
+	fChain->GetEntry(0); //carico una entries per poter prendere i valori dal file MC
+
 	int NPixX=480, NPixY=640;
 	if (SensorChoice==2) {
 		NPixX=488;
 		NPixY=648;
 	}
 	int NPixTot=NPixX*NPixY;
-//	int NPrimMC=1188000;
-	int NPrimMC=3600000;
-	double AttSorg=5.94e3; //Bq
+	//	int NPrimMC=1188000;
+	int NPrimMC=Nev;
+	double AttSorg=26.33e3; //Total activity of source [Bq] (was 5.94e3 maybe for Sr PG source)
 	double DTacq=200e-3; //Data Acquisition Time [s]
 	int NFramesMC=0, NFramesMCLow=0;
 	int FrameCounter=0;
-	int RedFactor=1;
+	int RedFactor=1, kk=0;
 	bool LowFlag=kFALSE;
+	bool debug=kFALSE;
 	
 	Long64_t nbytes = 0, nb = 0;
 	
@@ -57,68 +66,74 @@ void AnaCmosMC::Loop()
 	cout<<"Created output file: "<<FrameFile->GetName()<<endl;
 	cout<<"Created output file: "<<ImageFile->GetName()<<endl;
 	
-	NFramesMC=NPrimMC/AttSorg/0.2;  //is the number of "frame-equivalents" in the MC run
+	NFramesMC=NPrimMC/AttSorg/DTacq;  //is the number of "frame-equivalents" in the MC run
 	cout<<"#####################################"<<endl;
-	cout<<"Number of Primaries Generated in MC: "<<NPrimMC<<endl;
+	cout<<std::scientific<<"Number of Primaries Generated in MC: "<<(double)NPrimMC<<std::fixed<<endl;
 	cout<<"Source Activity [Bq]= "<<AttSorg<<endl;
-	cout<<"Entries = "<<nentries<<endl;
+	cout<<"Entries = "<<nentries<<endl; //is the number of events in which I have a signal
 	cout<<"It's like I'have simulated "<<NPrimMC/AttSorg<<" [s] of data taking, equivalent to about "<<NFramesMC <<" frames of 200 ms"<<endl;
+	cout<<"I will create a frame every "<<(int) nentries/NFramesMC<<" events, and append in case some empty ones" <<endl;
 	
 	if (nentries/NFramesMC == 0) {
-		
 		LowFlag=kTRUE;
-		NFramesMCLow=(NPrimMC/AttSorg*NFramesMC/nentries/0.2);
+		NFramesMCLow=(NPrimMC/AttSorg*NFramesMC/nentries/DTacq);
 		cout<<"ATTENTION! NUMBER OF EVENT TOO LOW FOR FRAME DIVISION, assuming 1 per frame and adding extra empty frames!"<<endl;
-		
 	}
 	
 	ImageFile->cd();
-	cluster=new TH2F("cluster","cluster", NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
-	double temp=0;
-	
+	TH2F* cluster =new TH2F("cluster","cluster", NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
+//	double temp=0;
+
+	int ContaPixel[NFramesMC];
+
 	TH2F* ClusterVec[NFramesMC];
-	ClusterVec[0]=new TH2F(Form("ClusterVec%d",0),"ClusterVec", NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
-
-
-	
+	for (kk=0; kk<NFramesMC; kk++){
+	ClusterVec[kk]=new TH2F(Form("ClusterVec%d",kk),Form("Cluster for frame n%d", kk), NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
+		ContaPixel[kk]=0;
+	}
+	//	ClusterVec[0]->Dump();
+	kk=0;
 	//############################################
 	//########## LOOP SU TUTTI GLI EVENTI
 	//#######
-	for (Long64_t jentry=0; jentry<nentries;jentry++) {
+	for (Long64_t jentry=0; jentry<nentries&&1;jentry++) { //Ciclo su tutti gli eventi MC (cioè Isotopo priario per Isotopo primario )
 		Long64_t ientry = LoadTree(jentry);
 		if (ientry < 0) break;
 		nb = fChain->GetEntry(jentry);   nbytes += nb;
-		
-		//	if (jentry==0) NPrimMC=Nev;
-		
-		temp+=EDepInPixel->size();
-//		cout<<"MERDA "<<EDepInPixel->size()<<" Somma= "<<temp<<endl;
-		
-		for (int ii=0; ii<PixelID->size(); ii++){  //loop su tutti i pixel
+		if (debug) cout<<endl;
+//		temp+=EDepInPixel->size();
+		ContaPixel[kk]+=PixelID->size(); //conto di quanti pixel accesi è fatto ciascun frame
+		//		cout<<"MERDA "<<EDepInPixel->size()<<" Somma= "<<temp<<endl;
+		for (int ii=0; ii<PixelID->size(); ii++){  //loop su tutti i pixel accesi durante quell'evento
+
+			//calcolo gli indici I e J per il pixel considerato: in questo modo vanno da 0 a NCol/NRow -1 come vuole Frame
+			int Ipix=((int)(PixelID->at(ii))-1)%NPixX;
+			int Jpix=((PixelID->at(ii)-1)/NPixX);
 			
-			int xpix=((int)(PixelID->at(ii)))%640;
-			int ypix=(PixelID->at(ii)/640);
-			//			frame->SetAdd(xpix, ypix, (EDepInPixel->at(ii))*1000);
-			frame->SetAdd(xpix, ypix, (int)(EDepInPixel->at(ii)*1000*ConvFactor));
-			//			cout<<"evento: "<<jentry<<"px: "<<PixelID->at(ii)<<", colonna= "<<xpix%640<<", x= "<<PixXPos->at(ii)<<", riga= "<<ypix<<", y= "<<PixYPos->at(ii)<<", ene= "<<EDepInPixel->at(ii)<<endl;
+			//			frame->SetAdd(Ipix, Jpix, (EDepInPixel->at(ii))*1000);
+			frame->SetAdd(Ipix, Jpix, (int)(EDepInPixel->at(ii)*1000*ConvFactor));
+			
+			if (debug) cout<<"evento: "<<jentry<<", px: "<<PixelID->at(ii)<<", colonna= "<<Ipix<<", x= "<<PixXPos->at(ii)<<", riga= "<<Jpix<<", y= "<<PixYPos->at(ii)<<", ene= "<<EDepInPixel->at(ii)<<", eneConv= "<<(int)(EDepInPixel->at(ii)*1000*ConvFactor)<<endl;
 			//			cout<<"evento: "<<jentry<<", ene= "<<EDepInPixel->at(ii)*1000<<" [keV], = "<<(int)(EDepInPixel->at(ii)*1000*ConvFactor)<<" [ADC]"<<endl;
-			cluster->Fill(xpix, ypix);
-//			cout<<"Xpix= "<<xpix;
-//			cout<<", Ypix= "<<ypix<<endl;
+			cluster->Fill(Ipix, Jpix);
+			//			cout<<"Ipix= "<<Ipix;
+			//			cout<<", Jpix= "<<Jpix<<endl;
 			
-			ClusterVec[FrameCounter]->Fill(xpix, ypix);
-//			ClusterVec[FrameCounter]->SetBinContent(xpix, ypix, 1);
+			ClusterVec[FrameCounter]->Fill(Ipix, Jpix);
+			//			ClusterVec[FrameCounter]->SetBinContent(Ipix, Jpix, 1);
 		}
 		//			cout<<"jentries: "<<jentry<<", nentries: "<<nentries<<", NFramesMC= "<<NFramesMC<<", nentries/NFramesMC= "<<nentries/NFramesMC<<", jentry%(nentries/NFramesMC)= "<<jentry%(nentries/NFramesMC)<<endl;
 		if (LowFlag || jentry%(nentries/NFramesMC+1)==0 ) {
+			if (FrameCounter%100==0) cout<<"Saving Frame Number: "<<FrameCounter<<endl;
 			frame->SetId(FrameCounter);
 			ImageFile->cd();
 			ClusterVec[FrameCounter]->Write();
-//			FrameFile->cd();
+			//			FrameFile->cd();
 			FrameCounter++;
-			ClusterVec[FrameCounter]=new TH2F(Form("ClusterVec%d",FrameCounter),"ClusterVec", NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
+//			ClusterVec[FrameCounter]=new TH2F(Form("ClusterVec%d",FrameCounter),"ClusterVec", NPixX/RedFactor, 0, NPixX, NPixY/RedFactor, 0, NPixY);
 			MCTree->Fill();
 			frame->Clear();
+			kk++; //contatore per il contatore dei pixel nel frame
 		}
 	}
 	
@@ -147,6 +162,10 @@ void AnaCmosMC::Loop()
 		}
 	}
 	
+	
+	for (kk=0; kk<NFramesMC; kk++){
+		if (1) cout<<"k= "<<kk<<", Counter "<<ContaPixel[kk]<<endl;;
+	}
 	FrameFile->Write();
 	ImageFile->cd();
 	cluster->Write();
--- AnaCmosMC.h
+++ AnaCmosMC.h
@@ -16,69 +16,80 @@
 #include "vector"
 
 class AnaCmosMC {
-public :
+	public :
 	TFile *FrameFile;
 	TFile *ImageFile;
-
-   TTree          *fChain;   //!pointer to the analyzed TTree or TChain
-   Int_t           fCurrent; //!current Tree number in a TChain
+	
+	TTree          *fChain;   //!pointer to the analyzed TTree or TChain
+	Int_t           fCurrent; //!current Tree number in a TChain
 	Double_t ConvFactor=10;
-
-// Fixed size dimensions of array or collections stored in the TTree if any.
-
-   // Declaration of leaf types
-   Double_t        Eabs;
-   Double_t        PreCmosTrackN;
-   vector<double>  *PreCmosPart;
-   vector<double>  *PreCmosEn;
-   Double_t        InCmosTrackN;
-   vector<double>  *InCmosPart;
-   vector<double>  *InCmosEn;
-   Double_t        InCmosEnPhot;
-   Double_t        InCmosEnEl;
-   Double_t        InCmosEnPos;
-   vector<double>  *PixelID;
-   vector<double>  *EDepInPixel;
-   vector<double>  *PixXPos;
-   vector<double>  *PixYPos;
-   Double_t        SourceX;
-   Double_t        SourceY;
-   Double_t        SourceZ;
-   Double_t        SourceEne;
-   Double_t        Nev;
-	TH2F* cluster;
-//	vector<TH2F*>  ClusterVec;
-
-   // List of branches
-   TBranch        *b_Eabs;   //!
-   TBranch        *b_PreCmosTrackN;   //!
-   TBranch        *b_PreCmosPart;   //!
-   TBranch        *b_PreCmosEn;   //!
-   TBranch        *b_InCmosTrackN;   //!
-   TBranch        *b_InCmosPart;   //!
-   TBranch        *b_InCmosEn;   //!
-   TBranch        *b_InCmosEnPhot;   //!
-   TBranch        *b_InCmosEnEl;   //!
-   TBranch        *b_InCmosEnPos;   //!
-   TBranch        *b_PixelID;   //!
-   TBranch        *b_EDepInPixel;   //!
-   TBranch        *b_PixXPos;   //!
-   TBranch        *b_PixYPos;   //!
-   TBranch        *b_SourceX;   //!
-   TBranch        *b_SourceY;   //!
-   TBranch        *b_SourceZ;   //!
-   TBranch        *b_SourceEne;   //!
-   TBranch        *b_Nev;   //!
-
-   AnaCmosMC(TString filename);
-   virtual ~AnaCmosMC();
-   virtual Int_t    Cut(Long64_t entry);
-   virtual Int_t    GetEntry(Long64_t entry);
-   virtual Long64_t LoadTree(Long64_t entry);
-   virtual void     Init(TTree *tree);
-   virtual void     Loop();
-   virtual Bool_t   Notify();
-   virtual void     Show(Long64_t entry = -1);
+	
+//	TH2F* cluster;	
+	// Fixed size dimensions of array or collections stored in the TTree if any.
+	// Declaration of leaf types
+	Double_t        Eabs;
+	Double_t        PreCmosTrackN;
+	vector<double>  *PreCmosPart;
+	vector<double>  *PreCmosEn;
+	Double_t        InCmosTrackN;
+	vector<double>  *InCmosPart;
+	vector<double>  *InCmosEn;
+	vector<double>  *InCmosX;
+	vector<double>  *InCmosY;
+	vector<double>  *InCmosZ;
+	Double_t        InCmosEnSr;
+	Double_t        InCmosEnY;
+	vector<double>  *PixelID;
+	vector<double>  *EDepInPixel;
+	vector<double>  *PixXPos;
+	vector<double>  *PixYPos;
+	Double_t        SourceX;
+	Double_t        SourceY;
+	Double_t        SourceZ;
+	Int_t           Nev;
+	vector<double>  *SourceCosX;
+	vector<double>  *SourceCosY;
+	vector<double>  *SourceCosZ;
+	vector<double>  *SourceEne;
+	vector<double>  *SourceIsotope;
+	
+	// List of branches
+	TBranch        *b_Eabs;   //!
+	TBranch        *b_PreCmosTrackN;   //!
+	TBranch        *b_PreCmosPart;   //!
+	TBranch        *b_PreCmosEn;   //!
+	TBranch        *b_InCmosTrackN;   //!
+	TBranch        *b_InCmosPart;   //!
+	TBranch        *b_InCmosEn;   //!
+	TBranch        *b_InCmosX;   //!
+	TBranch        *b_InCmosY;   //!
+	TBranch        *b_InCmosZ;   //!
+	TBranch        *b_InCmosEnSr;   //!
+	TBranch        *b_InCmosEnY;   //!
+	TBranch        *b_PixelID;   //!
+	TBranch        *b_EDepInPixel;   //!
+	TBranch        *b_PixXPos;   //!
+	TBranch        *b_PixYPos;   //!
+	TBranch        *b_SourceX;   //!
+	TBranch        *b_SourceY;   //!
+	TBranch        *b_SourceZ;   //!
+	TBranch        *b_Nev;   //!
+	TBranch        *b_SourceCosX;   //!
+	TBranch        *b_SourceCosY;   //!
+	TBranch        *b_SourceCosZ;   //!
+	TBranch        *b_SourceEne;   //!
+	TBranch        *b_SourceIsotope;   //!
+	
+	
+	AnaCmosMC(TString filename);
+	virtual ~AnaCmosMC();
+	virtual Int_t    Cut(Long64_t entry);
+	virtual Int_t    GetEntry(Long64_t entry);
+	virtual Long64_t LoadTree(Long64_t entry);
+	virtual void     Init(TTree *tree);
+	virtual void     Loop();
+	virtual Bool_t   Notify();
+	virtual void     Show(Long64_t entry = -1);
 };
 
 #endif
@@ -86,114 +97,127 @@ public :
 #ifdef AnaCmosMC_cxx
 AnaCmosMC::AnaCmosMC(TString filename) : fChain(0)
 {
-// if parameter tree is not specified (or zero), connect the file
-// used to generate this class and read the Tree.
-	
-	
 	TFile *f = new TFile(Form("%s.root",filename.Data()));
 	TTree *tree = (TTree*)gDirectory->Get("B1");
 	
+	//	f->ls();
 	FrameFile = new TFile(Form("%s_Frame%d.root",filename.Data(), (int)(ConvFactor*10)),"RECREATE");
 	ImageFile = new TFile(Form("%s_Image.root",filename.Data()),"RECREATE");
-//	FrameFile->cd();
+	//	FrameFile->cd();
 	Init(tree);
 }
 
 AnaCmosMC::~AnaCmosMC()
 {
-   if (!fChain) return;
-   delete fChain->GetCurrentFile();
+	if (!fChain) return;
+	delete fChain->GetCurrentFile();
 }
 
 Int_t AnaCmosMC::GetEntry(Long64_t entry)
 {
-// Read contents of entry.
-   if (!fChain) return 0;
-   return fChain->GetEntry(entry);
+	cout<<"MERDA"<<endl;
+	// Read contents of entry.
+	if (!fChain) return 0;
+	return fChain->GetEntry(entry);
 }
 Long64_t AnaCmosMC::LoadTree(Long64_t entry)
 {
-// Set the environment to read one entry
-   if (!fChain) return -5;
-   Long64_t centry = fChain->LoadTree(entry);
-   if (centry < 0) return centry;
-   if (fChain->GetTreeNumber() != fCurrent) {
-      fCurrent = fChain->GetTreeNumber();
-      Notify();
-   }
-   return centry;
+	// Set the environment to read one entry
+	if (!fChain) return -5;
+	Long64_t centry = fChain->LoadTree(entry);
+	if (centry < 0) return centry;
+	if (fChain->GetTreeNumber() != fCurrent) {
+		fCurrent = fChain->GetTreeNumber();
+		Notify();
+	}
+	return centry;
 }
 
 void AnaCmosMC::Init(TTree *tree)
 {
-   // The Init() function is called when the selector needs to initialize
-   // a new tree or chain. Typically here the branch addresses and branch
-   // pointers of the tree will be set.
-   // It is normally not necessary to make changes to the generated
-   // code, but the routine can be extended by the user if needed.
-   // Init() will be called many times when running on PROOF
-   // (once per file to be processed).
-
-   // Set object pointer
-   PreCmosPart = 0;
-   PreCmosEn = 0;
-   InCmosPart = 0;
-   InCmosEn = 0;
-   PixelID = 0;
-   EDepInPixel = 0;
-   PixXPos = 0;
-   PixYPos = 0;
-   // Set branch addresses and branch pointers
-   if (!tree) return;
-   fChain = tree;
-   fCurrent = -1;
-   fChain->SetMakeClass(1);
-
-   fChain->SetBranchAddress("Eabs", &Eabs, &b_Eabs);
-   fChain->SetBranchAddress("PreCmosTrackN", &PreCmosTrackN, &b_PreCmosTrackN);
-   fChain->SetBranchAddress("PreCmosPart", &PreCmosPart, &b_PreCmosPart);
-   fChain->SetBranchAddress("PreCmosEn", &PreCmosEn, &b_PreCmosEn);
-   fChain->SetBranchAddress("InCmosTrackN", &InCmosTrackN, &b_InCmosTrackN);
-   fChain->SetBranchAddress("InCmosPart", &InCmosPart, &b_InCmosPart);
-   fChain->SetBranchAddress("InCmosEn", &InCmosEn, &b_InCmosEn);
-   fChain->SetBranchAddress("InCmosEnPhot", &InCmosEnPhot, &b_InCmosEnPhot);
-   fChain->SetBranchAddress("InCmosEnEl", &InCmosEnEl, &b_InCmosEnEl);
-   fChain->SetBranchAddress("InCmosEnPos", &InCmosEnPos, &b_InCmosEnPos);
-   fChain->SetBranchAddress("PixelID", &PixelID, &b_PixelID);
-   fChain->SetBranchAddress("EDepInPixel", &EDepInPixel, &b_EDepInPixel);
-   fChain->SetBranchAddress("PixXPos", &PixXPos, &b_PixXPos);
-   fChain->SetBranchAddress("PixYPos", &PixYPos, &b_PixYPos);
-   fChain->SetBranchAddress("SourceX", &SourceX, &b_SourceX);
-   fChain->SetBranchAddress("SourceY", &SourceY, &b_SourceY);
-   fChain->SetBranchAddress("SourceZ", &SourceZ, &b_SourceZ);
-   fChain->SetBranchAddress("SourceEne", &SourceEne, &b_SourceEne);
-   fChain->SetBranchAddress("Nev", &Nev, &b_Nev);
-   Notify();
+	// The Init() function is called when the selector needs to initialize
+	// a new tree or chain. Typically here the branch addresses and branch
+	// pointers of the tree will be set.
+	// It is normally not necessary to make changes to the generated
+	// code, but the routine can be extended by the user if needed.
+	// Init() will be called many times when running on PROOF
+	// (once per file to be processed).
+	
+	// Set object pointer
+	PreCmosPart = 0;
+	PreCmosEn = 0;
+	InCmosPart = 0;
+	InCmosEn = 0;
+	InCmosX = 0;
+	InCmosY = 0;
+	InCmosZ = 0;
+	PixelID = 0;
+	EDepInPixel = 0;
+	PixXPos = 0;
+	PixYPos = 0;
+	SourceCosX = 0;
+	SourceCosY = 0;
+	SourceCosZ = 0;
+	SourceEne = 0;
+	SourceIsotope = 0;
+	// Set branch addresses and branch pointers
+	if (!tree) return;
+	fChain = tree;
+	fCurrent = -1;
+	fChain->SetMakeClass(1);
+	
+	
+	fChain->SetBranchAddress("Eabs", &Eabs, &b_Eabs);
+	fChain->SetBranchAddress("PreCmosTrackN", &PreCmosTrackN, &b_PreCmosTrackN);
+	fChain->SetBranchAddress("PreCmosPart", &PreCmosPart, &b_PreCmosPart);
+	fChain->SetBranchAddress("PreCmosEn", &PreCmosEn, &b_PreCmosEn);
+	fChain->SetBranchAddress("InCmosTrackN", &InCmosTrackN, &b_InCmosTrackN);
+	fChain->SetBranchAddress("InCmosPart", &InCmosPart, &b_InCmosPart);
+	fChain->SetBranchAddress("InCmosEn", &InCmosEn, &b_InCmosEn);
+	fChain->SetBranchAddress("InCmosX", &InCmosX, &b_InCmosX);
+	fChain->SetBranchAddress("InCmosY", &InCmosY, &b_InCmosY);
+	fChain->SetBranchAddress("InCmosZ", &InCmosZ, &b_InCmosZ);
+	fChain->SetBranchAddress("InCmosEnSr", &InCmosEnSr, &b_InCmosEnSr);
+	fChain->SetBranchAddress("InCmosEnY", &InCmosEnY, &b_InCmosEnY);
+	fChain->SetBranchAddress("PixelID", &PixelID, &b_PixelID);
+	fChain->SetBranchAddress("EDepInPixel", &EDepInPixel, &b_EDepInPixel);
+	fChain->SetBranchAddress("PixXPos", &PixXPos, &b_PixXPos);
+	fChain->SetBranchAddress("PixYPos", &PixYPos, &b_PixYPos);
+	fChain->SetBranchAddress("SourceX", &SourceX, &b_SourceX);
+	fChain->SetBranchAddress("SourceY", &SourceY, &b_SourceY);
+	fChain->SetBranchAddress("SourceZ", &SourceZ, &b_SourceZ);
+	fChain->SetBranchAddress("Nev", &Nev, &b_Nev);
+	fChain->SetBranchAddress("SourceCosX", &SourceCosX, &b_SourceCosX);
+	fChain->SetBranchAddress("SourceCosY", &SourceCosY, &b_SourceCosY);
+	fChain->SetBranchAddress("SourceCosZ", &SourceCosZ, &b_SourceCosZ);
+	fChain->SetBranchAddress("SourceEne", &SourceEne, &b_SourceEne);
+	fChain->SetBranchAddress("SourceIsotope", &SourceIsotope, &b_SourceIsotope);
+	Notify();
 }
 
 Bool_t AnaCmosMC::Notify()
 {
-   // The Notify() function is called when a new file is opened. This
-   // can be either for a new TTree in a TChain or when when a new TTree
-   // is started when using PROOF. It is normally not necessary to make changes
-   // to the generated code, but the routine can be extended by the
-   // user if needed. The return value is currently not used.
-
-   return kTRUE;
+	// The Notify() function is called when a new file is opened. This
+	// can be either for a new TTree in a TChain or when when a new TTree
+	// is started when using PROOF. It is normally not necessary to make changes
+	// to the generated code, but the routine can be extended by the
+	// user if needed. The return value is currently not used.
+	
+	return kTRUE;
 }
 
 void AnaCmosMC::Show(Long64_t entry)
 {
-// Print contents of entry.
-// If entry is not specified, print current entry
-   if (!fChain) return;
-   fChain->Show(entry);
+	// Print contents of entry.
+	// If entry is not specified, print current entry
+	if (!fChain) return;
+	fChain->Show(entry);
 }
 Int_t AnaCmosMC::Cut(Long64_t entry)
 {
-// This function may be called from Loop.
-// returns  1 if entry is accepted.
-// returns -1 otherwise.
-   return 1;
+	// This function may be called from Loop.
+	// returns  1 if entry is accepted.
+	// returns -1 otherwise.
+	return 1;
 }
 #endif // #ifdef AnaCmosMC_cxx
